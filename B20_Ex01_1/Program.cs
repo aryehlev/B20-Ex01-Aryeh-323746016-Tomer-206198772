fd523bca806c6f12a6ad4a226060a05bde43472b
Tomer <tomer2911@gmail.com>
2020-04-16 21:58:16 +0300

    GOOD NIGHT

--- B20_Ex01_1/Program.cs
+++ B20_Ex01_1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace B20_Ex01_1
 {
@@ -14,12 +15,23 @@ namespace B20_Ex01_1
                              "these are the numbers in decimal by order:{0} , {1} , {2}" ,
                              firstBinaryNumberFromUser, secondBinaryNumberFromUser, thirdBinaryNumberFromUser); 
             System.Console.WriteLine(messageDecimal);
-            byte averageNumberOfZeros(firstBinaryNumberFromUser, secondBinaryNumberFromUser, thirdBinaryNumberFromUser)
-            string messageDecimal = string.Format(
-                "these are the numbers in decimal by order:{0} , {1}",
-                averageNumberOfZeros(firstBinaryNumberFromUser,  secondBinaryNumberFromUser, thirdBinaryNumberFromUser), );
-
-
+            byte averageNumberOfZero = averageNumberOfZeros(
+                firstBinaryNumberFromUser,
+                secondBinaryNumberFromUser,
+                thirdBinaryNumberFromUser);
+            string messageAverage = string.Format(
+                "The average number of zeroes in all three numbers is: {0}{2}The average number of ones in all three numbers is:{1}",
+                averageNumberOfZero, 9 - averageNumberOfZero, System.Environment.NewLine);
+            System.Console.WriteLine(messageAverage);
+            byte countPowerOf2s = countPowerOf2s(
+                firstBinaryNumberFromUser,
+                secondBinaryNumberFromUser,
+                thirdBinaryNumberFromUser);
+            string messageAverage = string.Format(
+                "The average number of zeroes in all three numbers is: {0}{2}The average number of ones in all three numbers is:{1}",
+                averageNumberOfZero, 9 - averageNumberOfZero, System.Environment.NewLine);
+            
+            string messagePowerOf2 = string.Format("")
 
         }
 
@@ -65,8 +77,8 @@ namespace B20_Ex01_1
         private static byte averageNumberOfZeros(string i_FirstBinaryNumber, string i_SecondBinaryNumber, string i_ThirdBinaryNumber)
         {
             int numberOfZeros = 0;
-            string stringConcatination = string.Format("{0}{1}{2}", i_FirstBinaryNumber, i_SecondBinaryNumber, i_ThirdBinaryNumber);
-            foreach(char digit in stringConcatination)
+            string stringConcatenation = string.Format("{0}{1}{2}", i_FirstBinaryNumber, i_SecondBinaryNumber, i_ThirdBinaryNumber);
+            foreach(char digit in stringConcatenation)
             {
                 if(digit == '0')
                 {
@@ -74,17 +86,22 @@ namespace B20_Ex01_1
                 }
             }
 
-            return (byte)(numberOfZeros / stringConcatination.Length);
+            return (byte)(numberOfZeros / 3);
         }
 
-        private static byte averageNumberOfOnes()
+        private static byte countPowerOf2s(string i_FirstBinaryNumber, string i_SecondBinaryNumber, string i_ThirdBinaryNumber)
         {
-            return 0;
+            int count = 0;
+            count += isAPowerOf2(i_FirstBinaryNumber) ? 1 : 0;
+            count += isAPowerOf2(i_SecondBinaryNumber) ? 1 : 0;
+            count += isAPowerOf2(i_ThirdBinaryNumber) ? 1 : 0;
+            return (byte)count;
         }
 
-        private static byte countNumbersThatAreAPowerOf2(string[] i_Binary)
+
+        private static bool isAPowerOf2(string i_BinaryNumber)
         {
-            return 0;
+            return i_BinaryNumber.IndexOf('1') != i_BinaryNumber.LastIndexOf('1');
         }
 
 
