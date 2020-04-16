0f24630d06da0cb3a3497efbded9640559db1f0e
Tomer <tomer2911@gmail.com>
2020-04-16 19:26:08 +0300

    ex_01

--- B20_Ex01_1/Program.cs
+++ B20_Ex01_1/Program.cs
@@ -1,10 +1,39 @@
-﻿namespace B20_Ex01_1
+﻿using System;
+
+namespace B20_Ex01_1
 {
     public class Program
     {
         static void Main()
         {
-            System.Console.WriteLine("aaahh gigi");
+
         }
+
+        private static string getLegalBinaryInputFromUser()
+        {
+            return null;
+        }
+
+        private static ushort convertBinaryToDecimal(string i_BinaryStrToConvert)
+        {
+            return 0;
+        }
+
+        private static byte averageNumberOfZeros(ushort[] i_Binary)
+        {
+            return 0;
+        }
+
+        private static byte averageNumberOfOnes()
+        {
+            return 0;
+        }
+
+        private static byte countNumbersThatAreAPowerOf2(ushort[] i_Binary)
+        {
+            return 0;
+        }
+
+
     }
 }
